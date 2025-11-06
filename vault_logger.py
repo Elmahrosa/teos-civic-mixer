0433dbbf72980f2289ac580970b050b956a7b4b7
Ayman Seif <92527473+Aymanseif@users.noreply.github.com>
2025-11-06 22:43:31 +0200

    Update vault_logger.py

--- vault_logger.py
+++ vault_logger.py
@@ -1,3 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firebase_credentials.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 import json, firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import datetime, timezone, timedelta

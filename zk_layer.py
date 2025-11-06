a075b690177c983ea8b0903e3b60cb3323acd968
Ayman Seif <92527473+Aymanseif@users.noreply.github.com>
2025-11-06 22:19:01 +0200

    Update zk_layer.py

--- zk_layer.py
+++ zk_layer.py
@@ -1,1 +1,6 @@
+import hashlib, time
+
+def obfuscate(token_type: str, amount: float, destination_wallet: str = None) -> str:
+    secret_string = f"{token_type}-{amount}-{destination_wallet}-{time.time()}"
+    return hashlib.sha256(secret_string.encode()).hexdigest()
 
